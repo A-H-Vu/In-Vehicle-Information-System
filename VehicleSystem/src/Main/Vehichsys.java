2d6070db3004cec2d3a25cf9928dbfa3641a0d69
kekekekasper <77760923+kekekekasper@users.noreply.github.com>
2021-04-09 04:32:47 -0400

    Update Vehichsys.java
    Just pushed some raw codes, Still working for this

--- VehicleSystem/src/Main/Vehichsys.java
+++ VehicleSystem/src/Main/Vehichsys.java
@@ -4,48 +4,89 @@ import java.util.*;
 import java.util.Scanner;
 
 public class Vehichsys {
-	
-	private static HashMap<String,VehicleInfo> sys;
-	private static VehicleInfo v;
-	private String Customer;
-	private String brand;
-	private String model;
-	private String color;
-	private String platNum;
+		
+	private List<VehicleInfo> vehicles;
+	private List<CustomerInfo> customers;
+	private Map<CustomerInfo,Set<VehicleInfo>> OwnerInfo=new HashMap<CustomerInfo,Set<VehicleInfo>>();
 	
 	
-	public Vehichsys(int n) {
-		sys = new HashMap<String,VehicleInfo>(n);
+	public Vehichsys() {
+		vehicles=new ArrayList<VehicleInfo>();
+		customers=new ArrayList<CustomerInfo>();
+		
 	}
 	
-	public void SetVehicle(String b,String m,String c,String p) {
-		v.setBrand(b);
-		v.setModel(m);
-		v.setColor(c);
-		v.setPlatNum(p);
-	}
-	
-	public void Insertion(String name) {
-		this.Customer = name;
-		sys.put(Customer, v);
-	}
-	
-	public void Deletion(String name) {
-		sys.remove(name);
-	}
-	
-	public void Search(String name) {
-		if(sys.containsKey(name)) {
-			System.out.println(sys.get(name));
+	public void InsertVehicle(VehicleInfo ve) {
+		if(vehicles.contains(ve)) {
+			System.out.println("Error Message:This vehicle information already be contained");
+		}
+		if(ve==null) {
+			System.out.println("Error Message:Null vehicle information,Pleas input the information first");
 		}
 		else {
-			System.out.println("Sorry,the system did not find the "+name+"'s vehicle information");
+			vehicles.add(ve);
 		}
 	}
 	
-	public static void main(String[] args) {
-
+	public void DeleteVehicle(VehicleInfo ve) {
+		if(!vehicles.contains(ve)) {
+			System.out.println("Error Message:Not found the vehicle information,Please input the information first");
+		}
+		if(ve==null) {
+			System.out.println("Error Message:Null vehicle information,Pleas input the information first");
+		}
+		else {
+			vehicles.remove(ve);
+		}
 	}
 	
+	public void GetVehicle(VehicleInfo ve) {
+		if(!vehicles.contains(ve)) {
+			System.out.println("Error Message:Not found the vehicle information,Please input the information first");
+		}
+		if(ve==null) {
+			System.out.println("Error Message:Null vehicle information,Pleas input the information first");
+		}
+		else {
+			System.out.println(ve.toString());
+		}
+	}
 	
+	public void InsertCustomer(CustomerInfo cu) {
+		if(!customers.contains(cu)) {
+			System.out.println("Error Message:This customer information already be contained");
+		}
+		if(cu==null) {
+			System.out.println("Error Message:Null customer information,Pleas input the information first");
+		}
+		else {
+			customers.add(cu);
+		}
+	}
+	
+	public void DeleteCustomer(CustomerInfo cu) {
+		if(!customers.contains(cu)) {
+			System.out.println("Error Message:Not found the customer information,Please input the information first");
+		}
+		if(cu==null) {
+			System.out.println("Error Message:Null customer information,Pleas input the information first");
+		}
+		else {
+			customers.remove(cu);
+		}
+	}
+	
+	public void GetCustomer(CustomerInfo cu) {
+		if(!customers.contains(cu)) {
+			System.out.println("Error Message:Not found the customer information,Please input the information first");
+		}
+		if(cu==null) {
+			System.out.println("Error Message:Null customer information,Pleas input the information first");
+		}
+		else {
+			System.out.println(cu.toString());
+		}
+	}
+	
+
 }
