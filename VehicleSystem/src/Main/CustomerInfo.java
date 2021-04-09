012c0dac8713fc7ad9ceaaa873f5dcfc7e3609a0
kekekekasper <77760923+kekekekasper@users.noreply.github.com>
2021-04-09 04:34:58 -0400

    Create CustomerInfo.java

---
+++ VehicleSystem/src/Main/CustomerInfo.java
@@ -0,0 +1,41 @@
+package Main;
+
+public class CustomerInfo {
+	private String firstName;
+	private String lastName;
+	private String country;
+	
+	public CustomerInfo(String f,String l,String c) {
+		setFirstName(f);
+		setLastName(l);
+		setCountry(c);
+	}
+	
+	public void setFirstName(String f) {
+		this.firstName=f;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setLastName(String l) {
+		this.lastName=l;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setCountry(String co) {
+		this.country=co;
+	}
+	
+	public String getCountry() {
+		return country;
+	}
+	
+	public String toString() {
+		return "Name: "+firstName+" "+lastName+"/tCountry: "+country;
+	}
+}
