2ba8801ffbf4d9b6f9d22826787151a9cef449c8
kekekekasper <77760923+kekekekasper@users.noreply.github.com>
2021-04-08 23:30:08 -0400

    Delete Vehicle_Information_System_Map directory

--- Vehicle_Information_System_Map/src/main/Entry.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public interface Entry<K, V> {
-	K getKey();
-	V getValue();
-	void setKey(K k);
-	V setValue(V v);
-}
-
--- Vehicle_Information_System_Map/src/main/Iterator.java
+++
@@ -1,7 +0,0 @@
-package main;
-
-public interface Iterator<K> {
-	boolean hasNext();
-	K next();
-	void remove();	
-}
